996dcf747d9e7840d8842de8f207b97b1f64f9ab
unknown <shubhamdaule@gmail.com>
2023-07-23 22:55:40 -0500

    terraform module to request SSL certificate from ACM

--- website-ecs/main.tf
+++ website-ecs/main.tf
@@ -39,3 +39,12 @@ module "ecs-tasks-execution-role" {
   project_name = module.vpc.project_name
 
 }
+
+
+module "acm" {
+  source = "../modules/acm"
+  domain_name = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+
+
+}
